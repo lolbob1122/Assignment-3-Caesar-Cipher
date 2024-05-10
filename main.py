07d0b1feff6551dfad9e36f6d1d8be84d3c51a93
Luca P. Mattioli <lucapaolo.mattioli@gmail.com>
2024-05-10 17:43:17 +0200

    j

--- main.py
+++ main.py
@@ -2,65 +2,63 @@ import re
 import string
 alphabet = list(string.ascii_uppercase)
 running = True
+def readCode(filepath):
+    code_freq = []
+    with open(str(filepath)) as f: ### read the cipher
+        text = f.read()
+    letters_only = re.sub(r'[^a-zA-Z]', '', text)
+    letters_only = letters_only.upper()
+    ##print(list(letters_only)) ##DEBUG
+    for i in alphabet:
+        ## print(letters_only.count(i)) ## DEBUG
+        code_freq.append(letters_only.count(i))  ### find frequency of each letter
+    ##print(code_freq) ##DEBUG
+    total_code_freq = sum(int(item) for item in code_freq) ### find total frequency
+    ##print(total_code_freq) ##DEBUG
+    norm_code_freq = [round(float(code_freq[i]) / total_code_freq, 6) for i in range(len(code_freq))] ## find norm freq
+    ##print(norm_code_freq) ##DEBUG
+    return norm_code_freq
+def findShift(freqShifted):
+    ## print(alphabet) ##DEBUG
+    en_freq =[]
+    with open("Assignment-3-Caesar-Cipher\\ch-freq-en.txt") as f:
+        for line in f:    
+            en_freq.append(line.split())
+    total_en_freq = sum(float(item[1]) for item in en_freq) ### find total frequency
+    norm_en_freq = [[item[0], round(float(item[1])/ total_en_freq, 6) ] for item in en_freq] ### find normalized frequncy
+    norm_en_freq.sort() ### sort the list 
+    alphabet, norm_en_freq = zip(*norm_en_freq) 
+    ##print(freq)       ##DEBUG
+#  print(norm_en_freq)  ##DEBUG
+    shift = 0
+    smallestDiff = float('inf')
+    matched_letter = []
+    for j in range(len(freqShifted)):
+        SumDiff = sum(round(abs(norm_en_freq[i] - freqShifted[i-j]), 6) for i in range(len(freqShifted)))
+        if SumDiff < smallestDiff:
+            smallestDiff = SumDiff
+            shift = j
+            ##print(SumDiff) ##DEBUG
+            ##print(shift)
+    ##print(shift) ##DEBUG
+    return shift
+
+
+
+
 while running:
     deCode = input("Do you want to (de)code a file (Y/N)")
-    if deCode != 'Y' or deCode != 'N' or deCode != 'y' or deCode != 'n':
-        print('\nplease enter Y to (de)code a file or N to (de)code a text input')
-        continue
-    else:
-
-        def readCode(filepath):
-            code_freq = []
-            with open(str(filepath)) as f: ### read the cipher
-                text = f.read()
-            letters_only = re.sub(r'[^a-zA-Z]', '', text)
-            letters_only = letters_only.upper()
-            ##print(list(letters_only)) ##DEBUG
-            for i in alphabet:
-                ## print(letters_only.count(i)) ## DEBUG
-                code_freq.append(letters_only.count(i))  ### find frequency of each letter
-            ##print(code_freq) ##DEBUG
-            total_code_freq = sum(int(item) for item in code_freq) ### find total frequency
-            ##print(total_code_freq) ##DEBUG
-            norm_code_freq = [round(float(code_freq[i]) / total_code_freq, 6) for i in range(len(code_freq))] ## find norm freq
-            ##print(norm_code_freq) ##DEBUG
-            return norm_code_freq
-        def findShift(freqShifted):
-            ## print(alphabet) ##DEBUG
-            en_freq =[]
-            with open("Assignment-3-Caesar-Cipher\\ch-freq-en.txt") as f:
-                for line in f:    
-                    en_freq.append(line.split())
-            total_en_freq = sum(float(item[1]) for item in en_freq) ### find total frequency
-            norm_en_freq = [[item[0], round(float(item[1])/ total_en_freq, 6) ] for item in en_freq] ### find normalized frequncy
-            norm_en_freq.sort() ### sort the list 
-            alphabet, norm_en_freq = zip(*norm_en_freq) 
-            ##print(freq)       ##DEBUG
-        #  print(norm_en_freq)  ##DEBUG
-            shift = 0
-            smallestDiff = float('inf')
-            matched_letter = []
-            for j in range(len(freqShifted)):
-                SumDiff = sum(round(abs(norm_en_freq[i-j] - freqShifted[i]), 6) for i in range(len(freqShifted)))
-                if SumDiff < smallestDiff:
-                    smallestDiff = SumDiff
-                    shift = j
-                    ##print(SumDiff) ##DEBUG
-                    ##print(shift)
-            ##print(shift) ##DEBUG
-            return shift
-        # def cipher(text, shift):
-        #     if deCode == 'Y' or 'y' or 'yes' or 'Yes':
-                
-            
-        #     with open(str(text)) as f: ### read the cipher
-        #         text = f.read()
-
+    if deCode != 'N' or deCode != 'n':
+        break
+    elif deCode == 'y' or deCode == 'Y':
         norm_code_freq = readCode('Assignment-3-Caesar-Cipher\\codes\\secret0.txt')
         shift = findShift(norm_code_freq)
-
-
-
+        done = input("Do you want to run this program again? (Y/N)")
+        if done == 'n' or done == 'N':
+            running = False
+    else:
+        print('\nplease enter Y to (de)code a file or N to (de)code a text input')
+        continue
 
     ##TO-DO##
     # read code and determine frequencies
